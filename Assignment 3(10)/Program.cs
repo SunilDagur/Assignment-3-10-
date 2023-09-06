f2462b91b4fb8174b5530f5adaaab5b3001fd49a
Sunil Dagur <sunildagur566@gmail.com>
2023-09-06 22:32:34 +0530

    Add project files.

---
+++ Assignment 3(10)/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Reflection;
+
+namespace AssemblyA
+{
+    internal class MyClass
+    {
+        internal MyClass()
+        {
+            Console.WriteLine("Internal constructor called.");
+        }
+
+        internal void MyMethod()
+        {
+            Console.WriteLine("Internal method called.");
+        }
+    }
+}
+
+namespace AssemblyB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Assembly assemblyA = Assembly.LoadFile(@"Path\To\AssemblyA.dll");
+
+            
+            Type myClassType = assemblyA.GetType("AssemblyA.MyClass");
+
+            if (myClassType != null)
+            {
+                
+                object myClassInstance = Activator.CreateInstance(myClassType, nonPublic: true);
+
+                
+                MethodInfo myMethod = myClassType.GetMethod("MyMethod", BindingFlags.Instance | BindingFlags.NonPublic);
+                if (myMethod != null)
+                {
+                    myMethod.Invoke(myClassInstance, null);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Type not found in Assembly A.");
+            }
+        }
+    }
+}
